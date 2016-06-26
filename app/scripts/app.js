c73bed06c3d3f8b626c84140ba38674a74b0e362
Nick <nwarnke@unomaha.edu>
2016-06-25 22:02:44 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name budgetmanagementuiApp
+ * @description
+ * # budgetmanagementuiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('budgetmanagementuiApp', []);
