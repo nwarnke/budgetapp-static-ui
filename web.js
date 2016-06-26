e5f2db274256f83eb82300efbc0e8b3a21ed2bc1
Nick <nwarnke@unomaha.edu>
2016-06-26 00:46:04 -0500

    Add redirect for url

--- web.js
+++ web.js
@@ -6,8 +6,6 @@ app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
 
-
-
 app.get('/', function(req, res){
   res.redirect('/index.html');
 });
