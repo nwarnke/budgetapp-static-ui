d32e7b0a18e7d32512b10beb5b5d788730263925
nwarnke@unomaha.edu <erIamjh89>
2016-06-28 12:54:13 -0500

    Add css spinner when logging in

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,22 @@
 angular.module('permanenceStaticUiApp')
   .controller('MainCtrl', MainCtrl);
 
-function MainCtrl($http, $window) {
+function MainCtrl($http, $window, $timeout) {
   this.http = $http;
   this.window = $window;
+  this.timeout = $timeout;
   this.username = '';
   this.password = '';
   this.failedLogin = false;
-
+  this.pageLoading = false;
+  this.style = 'display:none';
 }
 
 MainCtrl.prototype.submitBtnEvt = function(){
   var vm = this;
+  this.pageLoading = true;
+  this.failedLogin = false;
+  this.style = '';
   this.http({
     method: 'POST',
     params: {'username':vm.username, 'password':vm.password},
@@ -23,11 +28,14 @@ MainCtrl.prototype.submitBtnEvt = function(){
     console.log(response.data);
     if(response.data.length > 0) {
       vm.window.location = '/#/welcome';
-    }else{
-      vm.failedLogin = true;
     }
+    vm.style = 'display:none';
+    vm.failedLogin = true;
   }, function errorCallback(error) {
     console.log('didn\'t work :(');
     console.log(error);
+    vm.pageLoading = false;
   });
+
 };
+
