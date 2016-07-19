8df10d40be6377787c9bf0dcf7d88d0212ab3967
Cole <cnardini@unomaha.edu>
2016-07-18 19:09:57 -0500

    MyAccount stuffs + navigation

--- app/scripts/controllers/myAccount.js
+++ app/scripts/controllers/myAccount.js
@@ -2,12 +2,24 @@
 angular.module('permanenceStaticUiApp')
   .controller('MyAccountCtrl', MyAccountCtrl);
 
-function MyAccountCtrl($rootScope) {
+function MyAccountCtrl($rootScope, $scope) {
+  this.scope = $scope;
+  this.changingPassword = false;
+  this.inEditMode = false;
   this.rootScope = $rootScope;
   this.rootScope.showNavBar = true;
+  this.initialize();
   console.log('welcome');
 }
 
+MyAccountCtrl.prototype.initialize = function() {
+  this.changingPassword = false;
+}
 
+MyAccountCtrl.prototype.clickChangePassword = function () {
+  this.changingPassword = true;
+};
 
-
+MyAccountCtrl.prototype.submitNewPassword = function () {
+  this.changingPassword = false;
+};
