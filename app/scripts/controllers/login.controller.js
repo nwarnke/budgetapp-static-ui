0530f7dac7ab3b13925e432d63e1625cd74848ec
Nick <nwarnke@unomaha.edu>
2016-07-20 15:20:21 -0500

    Prepare for heroku push

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -3,40 +3,15 @@
 angular.module('budgetApp')
   .controller('LoginCtrl', LoginCtrl);
 
-function LoginCtrl($http, $window, $timeout, $rootScope) {
-  this.http = $http;
-  this.window = $window;
-  this.rootScope = $rootScope;
-  this.timeout = $timeout;
-  this.username = '';
-  this.password = '';
-  this.failedLogin = false;
-  this.style = 'display:none';
+function LoginCtrl(Rest) {
+  this.rest = Rest;
+  this.username = null;
+  this.password = null;
 }
 
 LoginCtrl.prototype.submitBtnEvt = function(){
-  var vm = this;
-  this.failedLogin = false;
-  this.style = '';
-  this.http({
-    method: 'POST',
-    params: {'username':vm.username, 'password':vm.password},
-    url: 'https://budget-management-backend.herokuapp.com/login'
-  }).then(function successCallback(response) {
-    console.log('worked!');
-    console.log(response.data);
-    if(response.data.length > 0) {
-      vm.rootScope.showNavBar = true;
-      vm.window.location = '/#/BudgetsHome';
-    }else {
-      vm.style = 'display:none';
-      vm.failedLogin = true;
-    }
-  }, function errorCallback(error) {
-    console.log('didn\'t work :(');
-    console.log(error);
-    vm.style = 'display:none';
+  this.rest.isAuthUser(this.username, this.password).then(function(data){
+    console.log(data);
   });
-
 };
 
--- app/scripts/rest/rest.js
+++ app/scripts/rest/rest.js
@@ -4,15 +4,19 @@ angular.module('budgetApp')
   .factory('Rest', Rest);
 
 function Rest($http, $q){
-  var SERVICE_URL = 'Service URL goes here'; //TODO
+  var SERVICE_URL;
+  if(window.location.hostname == 'localhost'){
+    SERVICE_URL = 'http://localhost:8080';
+  }else{
+    SERVICE_URL = 'https://budget-management-backend.herokuapp.com';
+  }
   var performPost = function (url, params) {
     var deferred = $q.defer();
-    $http.post(
-      SERVICE_URL + url,
-      {
-        params: params
-      }
-    ).then(function (response) {
+    $http({
+      url:SERVICE_URL+'/login/authenticate',
+      method:'POST',
+      params:params
+    }).then(function (response) {
       deferred.resolve(response);
     })
       .catch(function (error) {
