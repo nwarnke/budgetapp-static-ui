7f9380568a4a7f6d6c2146949a01723557e8fdb9
Nick <nwarnke@unomaha.edu>
2016-07-22 17:21:40 -0500

    Add basic table to display database row

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -25,34 +25,9 @@ function HomeCtrl($rootScope, $scope, $cookies, Rest, $window) {
 
 
   this.scope = $scope;
-  this.budgets = [];
   this.rootScope = $rootScope;
   this.rootScope.showNavBar = true;
-  this.initialize();
-  console.log('welcome');
 }
-
-HomeCtrl.prototype.initialize = function(){
-  this.budgets = [{
-    name: 'Vacation Budget',
-    expenseTotal: 550,
-    totalLimit: 2000,
-    origDate: '7/12/2016'
-  },
-    {
-      name: 'Fixed Expenses Budget',
-      expenseTotal: 800,
-      totalLimit: 1000,
-      origDate: '7/12/2016'
-    },
-    {
-      name: 'Fixed Expenses Budget',
-      expenseTotal: 400,
-      totalLimit: 600,
-      origDate: '7/02/2016'
-    }]
-
-};
 
 HomeCtrl.prototype.goTo = function(link){
   var vm = this;
