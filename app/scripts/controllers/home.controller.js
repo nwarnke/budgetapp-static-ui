92b3d15fff54d1700c76b589f7f9a05c85806928
nwarnke@unomaha.edu <erIamjh89>
2016-07-22 12:58:29 -0500

    Begin displaying data on screen

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -2,18 +2,25 @@
 angular.module('budgetApp')
   .controller('HomeCtrl', HomeCtrl);
 
-function HomeCtrl($rootScope, $scope, Rest, $window) {
+function HomeCtrl($rootScope, $scope, $cookies, Rest, $window) {
   this.rest = Rest;
+  this.cookies = $cookies;
   this.window = $window;
   var vm = this;
-  // this.rest.isAuthenticatedToViewPage('/home/authenticated').then(function(data){
-  //   if(!data.data){
-  //     vm.window.location = '#/';
-  //   }
-  // });
 
-  this.rest.getBudgets().then(function(data){
-    console.log(data);
+  this.budgets = [];
+
+  this.rest.getBudgets().then(function(reply){
+    if(reply.status == 200) {
+      vm.budgets = reply.data;
+    }
+  }).catch(function(error){
+    if(error.status == 500){
+      console.log('server error occurred');
+    }else if(error.status == 401){
+      vm.cookies.put('authenticated', false);
+      vm.window.location = '#/';
+    }
   });
 
 
