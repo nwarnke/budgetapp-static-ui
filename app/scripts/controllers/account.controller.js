b54f32163280990d7dc4c70823bd6edd7c6e5300
Nick <nwarnke@unomaha.edu>
2016-08-15 22:21:17 -0500

    Add logout functionality to all pages

--- app/scripts/controllers/account.controller.js
+++ app/scripts/controllers/account.controller.js
@@ -2,8 +2,9 @@
 angular.module('budgetApp')
   .controller('AccountCtrl', AccountCtrl);
 
-function AccountCtrl($rootScope, $scope, Rest, $window) {
+function AccountCtrl($rootScope, $scope, $cookies, Rest, $window) {
   this.scope = $scope;
+  this.cookies = $cookies;
   this.changingPassword = false;
   this.rootScope = $rootScope;
   this.window = $window;
@@ -32,4 +33,13 @@ AccountCtrl.prototype.submitNewPassword = function () {
   else{
     vm.noMatch = true;
   }
+};
+
+AccountCtrl.prototype.logout = function(){
+  this.cookies.remove('authenticated');
+  var vm = this;
+  this.rest.logout().then(function(data){
+    vm.window.location = '#/';
+    console.log(data);
+  });
 };
